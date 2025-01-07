8aa8fb506c1c42af5e487ade0d3bf3389dbf8791
Mitch Ledwith <mitch@veliatx.com>
2025-01-07 15:45:37 -0800

    Refactor settings

--- expression_atlas_db/settings.py
+++ expression_atlas_db/settings.py
@@ -1,6 +1,6 @@
 from configparser import ConfigParser
 from pathlib import Path
-from sys import modules
+from typing import Dict
 import os
 
 settings_ini = (
@@ -16,49 +16,50 @@ if settings_ini.exists():
 else:
     raise Exception("No settings files at path: %s" % settings_ini)
 
-# set up the database connection string
-db_connection_string = "postgresql://%s:%s@%s/%s" % (
-    config["DATABASE"]["postgres_user"],
-    config["DATABASE"]["postgres_password"],
-    config["DATABASE"]["postgres_host"],
-    config["DATABASE"]["postgres_database"],
+def build_postgres_connection_string(user: str, password: str, host: str, database: str) -> str:
+    """Create a PostgreSQL connection string from components."""
+    return f"postgresql://{user}:{password}@{host}/{database}"
+
+def build_redshift_connection_string(user: str, password: str, url: str) -> str:
+    """Create a Redshift connection string from components."""
+    return f"redshift+psycopg2://{user}:{password}@{url}"
+
+# Database connection strings
+db_connection_string = build_postgres_connection_string(
+    user=config["DATABASE"]["postgres_user"],
+    password=config["DATABASE"]["postgres_password"],
+    host=config["DATABASE"]["postgres_host"],
+    database=config["DATABASE"]["postgres_database"],
 )
 
-# set up the test database connection string
-db_dev_connection_string = "postgresql://%s:%s@%s/%s" % (
-    config["DATABASE"]["postgres_user"],
-    config["DATABASE"]["postgres_password"],
-    config["DATABASE"]["postgres_host"],
-    config["DATABASE"]["postgres_test_database"],
+db_dev_connection_string = build_postgres_connection_string(
+    user=config["DATABASE"]["postgres_user"],
+    password=config["DATABASE"]["postgres_password"],
+    host=config["DATABASE"]["postgres_host"],
+    database=config["DATABASE"]["postgres_test_database"],
 )
 
-# set up the redshift connection string
-redshift_connection_string = "redshift+psycopg2://%s:%s@%s" % (
-    config["DATABASE"]["redshift_user"],
-    config["DATABASE"]["redshift_user"],
-    config["DATABASE"]["redshift_url"],
+# Redshift connection strings
+redshift_connection_string = build_redshift_connection_string(
+    user=config["DATABASE"]["redshift_user"],
+    password=config["DATABASE"]["redshift_user"], 
+    url=config["DATABASE"]["redshift_url"],
 )
 
-redshift_dev_connection_string = "redshift+psycopg2://%s:%s@%s" % (
-    config["DATABASE"]["redshift_user"],
-    config["DATABASE"]["redshift_user"],
-    config["DATABASE"]["redshift_test_url"],
+redshift_dev_connection_string = build_redshift_connection_string(
+    user=config["DATABASE"]["redshift_user"],
+    password=config["DATABASE"]["redshift_user"],
+    url=config["DATABASE"]["redshift_test_url"],
 )
 
-# set sqlite database connection string
-sqlite_connection_string = "sqlite:///%s" % (config["DATABASE"]["sqlite_test_database"])
+# SQLite connection string
+sqlite_connection_string = f"sqlite:///{config['DATABASE']['sqlite_test_database']}"
 
-# set test data location
+# Data locations
 test_experiment_loc = config["DATA"]["test_experiment_loc"]
-
-# set s3 data location
 s3_experiment_loc = config["DATA"]["s3_experiment_loc"]
-
-# set test data staging location
 test_staging_loc = config["DATA"]["test_staging_loc"]
-
-# set s3 staging location
 s3_staging_loc = config["DATA"]["s3_staging_loc"]
 
-# set redshift iam role
+# AWS configuration
 redshift_iam_role = config["DATA"]["redshift_iam_role"]
